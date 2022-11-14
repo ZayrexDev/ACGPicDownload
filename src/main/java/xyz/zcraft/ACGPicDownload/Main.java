d368f1100501ec005d6cb2a30ebea33af57b5944
zxzxy <478576442@qq.com>
2022-11-14 16:10:20 +0800

    updated usage

--- src/main/java/xyz/zcraft/ACGPicDownload/Main.java
+++ src/main/java/xyz/zcraft/ACGPicDownload/Main.java
@@ -197,7 +197,9 @@ public class Main {
                            -o, --output <output dictionary> : Set the output dictionary. Required.
                            --arg key1=value1,key2=value2,... : custom the argument in the url.
                                    Example:If the url is "https://www.someurl.com/pic?num=${num}", then with
-                                            "--arg num=1", the exact url will be "https://www.someurl.com/pic?num=1\""""
+                                            "--arg num=1", the exact url will be "https://www.someurl.com/pic?num=1\"
+                            --multi-thread : (Experimental) Enable multi thread download. May improve download speed.           
+                """
         );
     }
 }
