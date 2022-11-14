fb32861ef681047a3f0c196b5b5dee06ba1d686c
zxzxy <478576442@qq.com>
2022-11-14 16:08:41 +0800

    deleted tests

--- src/test/java/Test.java
+++
@@ -1,7 +0,0 @@
-import xyz.zcraft.ACGPicDownload.Main;
-
-public class Test {
-    public static void main(String[] args) {
-        Main.main("--arg num=20 --multi-thread -o pic".split(" "));
-    }
-}
