409e8e0de88b9b58e7d8f2c9ea453488efc4eaf1
zxzxy <478576442@qq.com>
2022-11-18 11:40:23 +0800

    completed #5 and updated readme

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,7 +3,7 @@ import xyz.zcraft.ACGPicDownload.Main;
 public class Test {
     public static void main(String[] args) {
         // System.out.println("asd.ds".substring(0, "asd.ds".lastIndexOf(".") + 1).concat("json"));
-        Main.main("-s dmoe".split(" "));
-        // Main.main("--arg num=20 -o pic --multi-thread".split(" "));
+        // Main.main("-s dmoe".split(" "));
+        Main.main("--arg num=20 -o pic --multi-thread -t 5".split(" "));
     }
 }
